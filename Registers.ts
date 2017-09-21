78394eb02076296ab0ba97fc33bbe1b65465da26
Horhof <hor@hor.hh>
2017-09-21 12:49:36 -0400

    up

---
+++ Registers.ts
@@ -0,0 +1,28 @@
+import * as Debug from 'debug'
+
+import { Word, Addr } from './Word'
+
+const log = Debug('Mel:Registers')
+
+export class Registers {
+  public readonly table: { [name: string]: Addr } = {
+    accum: null,
+    data: null,
+    count: null,
+    dest: null,
+    src: null,
+    input: null,
+    output: null,
+    stack: null,
+    base: null
+  }
+
+  static MAX = 9
+
+  constructor(memory: Word[]) {
+    const names = Object.keys(this.table)
+    for (let i = 0; i < Registers.MAX; i++) {
+      this.table[names[i]] = new Addr(i, memory)
+    }
+  }
+}
