b8c214cc1caef355bbb9def92708d302a94331e1
Horhof <hor@hor.hh>
2017-09-30 10:35:43 -0400

    up

--- Word.ts
+++ Word.ts
@@ -6,7 +6,7 @@
  * - Immediate
  * - DataAddress
  * - InstructionAddress
- * - Ptr
+ * - Pointer
  * - Channel
  * - Port
  */
@@ -18,6 +18,17 @@ const io = Debug('Mel:I/O')
 export type Word = number
 export type Label = string
 
+/**
+ * An operand of an operation representing some component of the VM being
+ * interacted with. This is an abstract class extended by the actual
+ * arguments.
+ *  
+ * API:
+ * - Data
+ * - Summary
+ * - Read = word of data
+ * - Write: word.
+ */
 class Argument {
   /**
    * I am a wrapper around a single piece of data.
@@ -32,13 +43,14 @@ class Argument {
   static ZERO = 0
 
   constructor(data: number, memory?: Word[]) {
-    this.data = data || Immediate.ZERO
+    this.data = data || Argument.ZERO
     this.memory = memory || []
   }
 
   /** A human-readable representation of this argument. */
   public get summary(): string {
-    return `Immediate ${this.data}`
+    // Overridden.
+    return ``
   }
 
   public read(): Word {
@@ -46,55 +58,27 @@ class Argument {
   }
 
   public write(value: Word): void {
-    throw new Error(`Error. Immediate values are immutable.`)
+    // Overridden.
   }
 }
 
 /**
- * An operand whose value is directly held inside the instance.
- *  
- * API:
- * - Data
- * - Summary
- * - Read = word of data
- * - Write: word.
+ * I am an operand whose data value is directly held. The data is a compile
+ * time constant directly from the instructiosn in the code.
  */
-export class Immediate {
-  /**
-   * I am a wrapper around a single piece of data.
-   */
-  public readonly data: number
-
-  /** 
-   * Memory is used for addresses/pointers but not for immediate values.
-   */
-  protected readonly memory: Word[]
-
-  static ZERO = 0
-
-  constructor(data: number, memory?: Word[]) {
-    this.data = data || Immediate.ZERO
-    this.memory = memory || []
-  }
-
-  /** A human-readable representation of this argument. */
+export class Immediate extends Argument {
   public get summary(): string {
     return `Immediate ${this.data}`
   }
 
-  public read(): Word {
-    return this.data
-  }
-
   public write(value: Word): void {
-    throw new Error(`Error. Immediate values are immutable.`)
+    throw new Error(`Error: immediate values are immutable.`)
   }
 }
 
 /**
- * An operand whose value is the address where the value is held
- * and needs to be read and can be written.
-
+ * I am an operand pointing to a memory address. Operations will read and write
+ * to the value inside that address.
  */
 export class DataAddress extends Immediate {
   public get address(): number {
@@ -106,7 +90,7 @@ export class DataAddress extends Immediate {
   }
 
   public read(): Word {
-    return this.memory[this.address] || Immediate.ZERO
+    return this.memory[this.address] || Argument.ZERO
   }
 
   public write(value: Word): void {
@@ -114,15 +98,20 @@ export class DataAddress extends Immediate {
   }
 }
 
+/**
+ * I am an operand pointing to an instruction. Operations will use operands
+ * like these when doing jumps.
+ */
 export class InstructionAddress extends Immediate {
   public get summary(): string {
-    return `Op #${this.data}`
+    return `Instruction #${this.data}`
   }
 }
 
 /**
- * An operand whose value is the address that itself contains an
- * address where the value is held.
+ * I am an operand pointing to a memory address but the value at that address
+ * is not directly use. Instead operations will use the value as another
+ * address and do a second memory access to get it.
  */
 export class Pointer extends DataAddress {
   public get address(): number {
@@ -134,6 +123,14 @@ export class Pointer extends DataAddress {
   }
 }
 
+/**
+ * I represent a queue of words coming from and going to the outside of the
+ * program.
+ * 
+ * API:
+ * - push: word.
+ * - pull = word
+ */
 export class Channel {
   public data: Word[]
 
@@ -154,8 +151,8 @@ export class Channel {
 }
 
 /**
- * The "memory" for a port are the I/O channels. It will write to whichever
- * one its address points to.
+ * I am an address not for a memory location but an I/O channel. I read from
+ * and write to the channel's queue.
  */
 export class Port extends DataAddress {
   private readonly channels: Channel[]
