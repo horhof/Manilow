6d5d0e10c03201b944a75edda70269832dcf4391
Horhof <hor@hor.hh>
2018-01-28 15:01:45 -0500

    Rename to Variable test and test writing

--- test/Variable_test.ts
+++ test/Variable_test.ts
@@ -16,11 +16,25 @@ describe(`Variable`, () => {
     memory = new Memory()
   })
 
+  it(`should have an address`, () => {
+    const variable = new Variable('var', 3, memory)
+    expect(variable.address).to.equal(3)
+  })
+
   it(`should read data from memory`, () => {
     const address = 3
     const data = 18
     memory.set(address, data)
     const variable = new Variable('var', address, memory)
     expect(variable.read()).to.equal(data)
+  })
+
+  it(`should write data to memory`, () => {
+    const address = 3
+    const data = 18
+    memory.set(address, data)
+    const variable = new Variable('var', address, memory)
+    variable.write(0)
+    expect(variable.read()).to.equal(0)
   })
 })
