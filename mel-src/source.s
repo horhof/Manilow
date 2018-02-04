4c8bc506e7fd4f4a49744847d8fd884d0dff6373
Horhof <hor@hor.hh>
2018-02-04 13:10:48 -0500

    Remove old mel-src directory

--- mel-src/source.s
+++
@@ -1,9 +0,0 @@
-  GOTO ProgramStart
-SubRoutine:
-  MUL 0d10;         ; Multiple the accumulator value.
-  EXIT @accum
-ProgramStart:
-  COPY 0d5          ; Put 5 into the accumulator.
-  ENTER SubRoutine
-  ENTER SubRoutine
-  HALT
