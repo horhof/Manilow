10ee3077e6d6b8177a831869f1a04fb8554cb040
Horhof <hor@hor.hh>
2018-02-04 15:01:15 -0500

    Add ZERO test

--- test/DataOps_test.ts
+++ test/DataOps_test.ts
@@ -1,7 +1,3 @@
-/**
- * Test data operations.
- */
-
 import { expect, log } from './setup'
 import { Machine } from '../src/Machine'
 
@@ -10,24 +6,35 @@ describe(`Data operations`, () => {
 
   beforeEach(() => vm = new Machine())
 
-  it(`should nullary copy from data to accumulator`, () =>
-    expect(vm.run(`
-      COPY 0d9, @accum  ; Initialize accumulator to 9.
-      COPY 0d7, @data   ; Put 7 in data.
-      COPY              ; Copy the 7 in data to accumulator.
-    `)
-      .then(() => vm.bus.accum.read())).to.eventually.equal(7))
+  describe(`COPY`, () => {
+    it(`should nullary copy from data to accumulator`, () =>
+      expect(vm.run(`
+        COPY 0d9, @accum  ; Initialize accumulator to 9.
+        COPY 0d7, @data   ; Put 7 in data.
+        COPY              ; Copy the 7 in data to accumulator.
+      `)
+        .then(() => vm.bus.accum.read())).to.eventually.equal(7))
 
-  it(`should unary copy to accumulator`, () =>
-    expect(vm.run(`
-      COPY 0d7
-    `)
-      .then(() => vm.bus.accum.read())).to.eventually.equal(7))
+    it(`should unary copy to accumulator`, () =>
+      expect(vm.run(`
+        COPY 0d7
+      `)
+        .then(() => vm.bus.accum.read())).to.eventually.equal(7))
 
-  it.only(`should binary copy`, () =>
-    expect(vm.run(`
-      DEF 0d50, &dest
-      COPY 0d7, @dest
-    `)
-      .then(() => vm.bus.memory.get(50))).to.eventually.equal(7))
+    it(`should binary copy`, () =>
+      expect(vm.run(`
+        DEF 0d50, &dest
+        COPY 0d7, @dest
+      `)
+        .then(() => vm.bus.memory.get(50))).to.eventually.equal(7))
+  })
+
+  describe(`ZERO`, () => {
+    it(`should nullary zero the accumulator`, () =>
+      expect(vm.run(`
+        COPY 0d77
+        ZERO
+      `)
+        .then(() => vm.bus.accum.read())).to.eventually.equal(0))
+  })
 })
