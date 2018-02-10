c07a1198585ba3c9faf614a7d99cc125008f2cb2
Horhof <hor@hor.hh>
2018-02-09 22:27:39 -0500

    Update line parser tests

--- test/LineParser_test.ts
+++ test/LineParser_test.ts
@@ -1,7 +1,7 @@
 import { expect, log } from './setup'
 import { Line, LineType } from '../src/parser/Line';
 
-describe.only(`Line parser`, () => {
+describe(`Line parser`, () => {
   let line: Line
 
   it(`should parse empty lines`, () =>
@@ -10,9 +10,15 @@ describe.only(`Line parser`, () => {
   it(`should parse spaces as empty lines`, () =>
     expect(new Line(`    `).type).to.equal(LineType.EMPTY))
 
-  it(`should parse one-line comments`, () =>
-    expect(new Line(`; This should be called on each loop.`).type).to.equal(LineType.EMPTY))
+  it(`should parse one-line comments`, () => {
+    const line = new Line(`; This should be called on each loop.`)
+    expect(line.type).to.equal(LineType.EMPTY)
+    expect(line.comment).not.to.be.empty
+  })
 
-  it(`should parse one-line comments which have extra spaces`, () =>
-    expect(new Line(`  ; This should be called on each loop.  `).type).to.equal(LineType.EMPTY))
+  it(`should parse one-line comments which have extra spaces`, () => {
+    const line = new Line(`  ; This should be called on each loop.  `)
+    expect(line.type).to.equal(LineType.EMPTY)
+    expect(line.comment).not.to.be.empty
+  })
 })
